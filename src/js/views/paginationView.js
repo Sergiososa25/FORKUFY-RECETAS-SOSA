8bce5783924d5b69b5a47aabc9b02f994d6b71ed
Sergiososa25 <chuki25sosa@gmail.com>
2026-09-06 13:30:57 -0600

    Proyecto final Forkify completo

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,67 @@
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView {
+  #parentElement = document.querySelector('.pagination');
+  #data;
+
+  addHandlerClick(handler) {
+    this.#parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  render(data) {
+    this.#data = data;
+    const numPages = Math.ceil(this.#data.results.length / this.#data.resultsPerPage);
+    const curPage = this.#data.page;
+
+    // Página 1 y hay más páginas
+    if (curPage === 1 && numPages > 1) {
+      return this.#generateButtonNext(curPage);
+    }
+
+    // Última página
+    if (curPage === numPages && numPages > 1) {
+      return this.#generateButtonPrev(curPage);
+    }
+
+    // Página intermedia
+    if (curPage < numPages) {
+      return `
+        ${this.#generateButtonPrev(curPage)}
+        ${this.#generateButtonNext(curPage)}
+      `;
+    }
+
+    // Página 1 y NO hay más páginas
+    return '';
+  }
+
+  #generateButtonPrev(curPage) {
+    return `
+      <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${curPage - 1}</span>
+      </button>
+    `;
+  }
+
+  #generateButtonNext(curPage) {
+    return `
+      <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
+        <span>Page ${curPage + 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>
+    `;
+  }
+}
+
+export default new PaginationView();
